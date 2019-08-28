7c131c77170bc90e3a690d7c19fd2347f4950707
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-29 03:31:15 +0530

    appId Update

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -45,7 +45,7 @@ export default class Facebook extends Component {
     } else {
       fbContent = (
         <FacebookLogin
-          appId='2443606162393349'
+          appId='Your Appid'
           autoLoad={true}
           fields='name,email,picture'
           onClick={this.componentClicked}
