4e078e3a4f330636670a2452697cb61787d427f4
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-29 03:29:22 +0530

    add fb-login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Facebook from "./components/Facebook";
 function App() {
   return (
     <div className='App'>
+      <h1>Welcome To Facebook Authentication </h1>
       <Facebook />
     </div>
   );
